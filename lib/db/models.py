499215bd0a594d149f12a4e8297683c8827d4c14
felix maati <maatifelix001@gmail.com>
2024-09-20 08:35:50 +0300

    seeds

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,36 +3,28 @@ from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
 Base = declarative_base()
 
-# Define a database URL (you can use SQLite for simplicity)
-DATABASE_URL = 'sqlite:///book_store.db'
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine("sqlite:///bookstore.db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Author model
+# Models
 class Author(Base):
     __tablename__ = 'authors'
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
-    books = relationship('Book', back_populates='author')
+    name = Column(String, nullable=False)
+    books = relationship("Book", back_populates="author")
 
-# Book model
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     author_id = Column(Integer, ForeignKey('authors.id'))
-    author = relationship('Author', back_populates='books')
+    author = relationship("Author", back_populates="books")
+    sales = relationship("Sale", back_populates="book")
 
-# Sale model
 class Sale(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=False)
     book_id = Column(Integer, ForeignKey('books.id'))
-    quantity = Column(Integer, default=1)
-    book = relationship('Book')
-
-# Initialize the database
-def initialize_database():
-    Base.metadata.create_all(engine)
+    book = relationship("Book", back_populates="sales")
